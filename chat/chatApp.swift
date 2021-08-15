48695ad5720a544cb8830c1784acb80aa280d0a5
Nazar Kahaniak <nazar.k@dreambit.io>
2021-08-15 22:49:46 +0300

    Initial commit

---
+++ chat/chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatApp.swift
+//  chat
+//
+//  Created by Nazar on 15.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
